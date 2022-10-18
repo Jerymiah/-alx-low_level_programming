fc5ca35999a976c89510c6d0ad86de19044eac81
Jerymiah <jeremiahnkwocha4@gmail.com>
2022-10-18 12:41:18 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- * main - print _putchar
- * is written by jeremiah
- * return:0
+ * main - print "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char *sh = "_putchar";
+
+	while (*sh)
+	{
+		_putchar (*sh);
+		sh++;
+	}
+	_putchar ('\n');
+
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,10 @@
-int _putchar(char c);
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+
+char _putchar(char c);
 
 void print_alphabet(void);
 
@@ -12,14 +18,18 @@ int print_sign(int n);
 
 int _abs(int);
 
-int print_last_digit(int);
+int print_last_digit(int nld);
 
 void jack_bauer(void);
 
 void times_table(void);
 
-int add(int, int);
+int add(int one, int two);
 
 void print_to_98(int n);
 
 void print_times_table(int n);
+
+
+
+#endif /* MAIN_H */
